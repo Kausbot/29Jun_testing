50e93308bb10726862fd599551e50ab84700fb30
Kausbot <f2014420@goa.bits-pilani.ac.in>
2016-06-29 20:17:45 +0530

    Initial commit

---
+++ app/src/main/java/ps1/bits_piliani/a29jun_testing/MainActivity.java
@@ -0,0 +1,13 @@
+package ps1.bits_piliani.a29jun_testing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
